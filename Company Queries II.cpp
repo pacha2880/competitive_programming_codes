507167f06f3abd03ebcf8a305c4538ed1d34df84
pacha2880 <pacha2881@gmail.com>
2025-09-12 20:34:30 +0200

    cses

---
+++ Company Queries II.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/rope>
+#define int ll
+#define mp				make_pair
+#define pb				push_back
+#define all(a)			(a).begin(), (a).end()
+#define sz(a)			(int)a.size()
+#define eq(a, b)     	(fabs(a - b) < EPS)
+#define md(a, b)		((a) % b + b) % b
+#define mod(a)			md(a, MOD)
+#define _max(a, b) ((a) > (b) ? (a) : (b))
+#define srt(a)			sort(all(a))
+#define mem(a, h)		memset(a, (h), sizeof(a))
+#define f 				first
+#define s 				second
+#define forn(i, n)			for(int i = 0; i < n; i++)
+#define fore(i, b, e)	for(int i = b; i < e; i++)
+#define forg(i, b, e, m)	for(int i = b; i < e; i+=m)
+#define index	int mid = (b + e) / 2, l = node * 2 + 1, r = l + 1;
+#define DBG(x) cerr<<#x<<" = "<<(x)<<endl
+#define RAYA cout<<"=============================="<<'\n'
+// int in(){int r=0,c;for(c=getchar();c<=32;c=getchar());if(c=='-') return -in();for(;c>32;r=(r<<1)+(r<<3)+c-'0',c=getchar());return r;}
+
+
+using namespace std;
+// using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+// #pragma GCC optimization ("O2")
+// #pragma GCC optimize("Ofast") si el O3 no da
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+typedef long long 		ll;
+typedef long double ld;	
+typedef unsigned long long 		ull;
+typedef pair<int, int>  ii;
+typedef pair<pair<int, int>, int> iii;
+typedef vector<int>     vi;
+typedef vector<ii>      vii;
+typedef vector<ll>      vll;
+typedef vector<vector<int>> mat;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// find_by_order kth largest  order_of_key <
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// rng
+const int tam = 200010;
+const int MOD = 1000000007;
+const int MOD1 = 998244353;
+const double DINF=1e100;
+const double EPS = 1e-9;
+const double PI = acos(-1); 
+vi g[tam];
+int pro[tam];
+void dfs(int node, int prof = 0)
+{
+    pro[node] = prof;
+    for(int x : g[node])
+        dfs(x, prof + 1);
+}
+signed main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// freopen("asd.txt", "r", stdin);
+	// freopen("qwe.txt", "w", stdout); 
+	int n, q;
+    cin>>n>>q;
+    mat pa(20, vi(n));
+    fore(i, 1, n) cin>>pa[0][i], pa[0][i]--, g[pa[0][i]].pb(i);
+    fore(j, 1, 20)
+        fore(i, 0, n)
+            pa[j][i] = pa[j - 1][pa[j - 1][i]];
+    dfs(0);
+    while(q--)
+    {
+        int a, b;
+        cin>>a>>b;
+        a--, b--;
+        if(pro[a] < pro[b]) swap(a, b);
+        int subir = pro[a] - pro[b];
+        for(int i = 19; i > -1; i--)
+            if((1<<i) <= subir)
+                subir -= 1<<i, a = pa[i][a];
+        if(a == b)
+        {
+            cout<<a + 1<<'\n';
+            continue;
+        }
+        for(int i = 19; i > -1; i--)
+            if(pa[i][a] != pa[i][b])
+                a = pa[i][a], b = pa[i][b];
+        cout<<pa[0][a] + 1<<'\n';
+    }
+	return 0;
+}
+// Se vuelve más fácil,
+// cada día es un poco más fácil, pero tienes que hacerlo cada día,
+// es la parte difícil, pero se vuelve más fácil.
+// Crecer duele.
+// La única manera de pasar esa barrera es pasandola.
+// No sirve de nada hacer sacrificios si no tienes disciplina.
+// Cae 7 veces, levántate 8.
+// LA DISCIPLINA es el puente entre tus metas y tus logros.
+// Las indisciplinadas son mi debilidad
+// Take a sad song and make it better
